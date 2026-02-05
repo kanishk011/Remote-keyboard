994adf2f1a9524b049aa3bac598028336cc79ea6
kanishk011 <itskanishk11@gmail.com>
2026-02-05 09:56:48 +0530

    Enabled cors and configured

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import keyboardRouter from './modules/keyboard/keyboard.router.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 // Routes
